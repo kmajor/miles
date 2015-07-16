56af74e405b99f0a499d20618062b76dfd61b694
Kevin Major <kevin@commercekitchen.com>
2015-07-16 16:02:43 -0600

    some cleanup to get site working locally on loveland

--- db/seeds/airports.rb
+++ db/seeds/airports.rb
@@ -4,11 +4,11 @@
 file = File.read('airport_codes/airports.json')
 airport_codes = JSON.parse(file)
 airport_codes.each do |airport|
- airport[1]['openflight_id'] = airport[0] 
+ airport[1]['openflight_id'] = airport[0]
  Airport.new(airport[1]).save
 end
 
-#rails g scaffold airlines openflight_id:string name:string alias:string iata:string iaco:string callsign:string country:string active:boolean      
+#rails g scaffold airlines openflight_id:string name:string alias:string iata:string iaco:string callsign:string country:string active:boolean
 airlines=[{name: 'United', alias: "UA"}, {name:'American', alias:"AA"}, {name: 'Delta', alias:"DELTA"}]
 airlines.each do |airline|
  Airline.create(airline).save
